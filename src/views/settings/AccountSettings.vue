9c333c2095de6d288505fb05afc5be987bffb47a
enqer <pawel.kam4@gmail.com>
2024-09-05 22:17:08 +0200

    feat: account settings

--- src/views/settings/AccountSettings.vue
+++ src/views/settings/AccountSettings.vue
@@ -1,8 +1,25 @@
 <script setup lang="ts">
 import PageHeader from '@/components/PageHeader.vue';
+import { ref } from 'vue';
+
+const isPolishLang = ref(true);
 </script>
 <template>
   <main>
     <page-header label="konto" />
+    <div class="tw-flex tw-flex-col tw-gap-y-10">
+      <div class="tw-flex tw-gap-x-10">
+        <div class="tw-w-1/3">
+          <span class="tw-text-base">Język</span>
+        </div>
+        <div class="q-gutter-sm tw-flex tw-flex-col">
+          <q-radio v-model="isPolishLang" :val="true" label="Polski" />
+          <q-radio v-model="isPolishLang" :val="false" label="Angielski" />
+        </div>
+      </div>
+      <div class="tw-flex tw-gap-x-10">
+        <q-btn label="Usuń konto" color="primary" />
+      </div>
+    </div>
   </main>
 </template>
--- src/views/settings/NotifySettings.vue
+++ src/views/settings/NotifySettings.vue
@@ -11,7 +11,7 @@ const isAutoDeleteNotify = ref(false);
     <div class="tw-flex tw-flex-col tw-gap-y-10">
       <div class="tw-flex tw-gap-x-10">
         <div class="tw-w-1/3">
-          <span>Wyświetlanie powiadomień</span>
+          <span class="tw-text-base">Wyświetlanie powiadomień</span>
         </div>
         <div class="q-gutter-sm tw-flex tw-flex-col">
           <q-radio v-model="isNotifyTurnOn" :val="true" label="Włącz" />
@@ -20,7 +20,9 @@ const isAutoDeleteNotify = ref(false);
       </div>
       <div class="tw-flex tw-gap-x-10">
         <div class="tw-w-1/3">
-          <span>Automatycznie usuwaj powiadomienia po przeczytaniu</span>
+          <span class="tw-text-base">
+            Automatycznie usuwaj powiadomienia po przeczytaniu
+          </span>
         </div>
         <div class="q-gutter-sm tw-flex tw-flex-col">
           <q-radio v-model="isAutoDeleteNotify" :val="true" label="Włącz" />
