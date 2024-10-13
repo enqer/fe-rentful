0a7fff6f603e23f88f85b87a5457d4c6b65cc119
enqer <pawel.kam4@gmail.com>
2024-10-13 21:44:27 +0200

    feat: toggle option

---
+++ src/components/apartments/ToggleOption.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+defineProps({
+  label: {
+    type: String,
+    required: true,
+  },
+  icon: {
+    type: String,
+    required: true,
+  },
+});
+const switched = defineModel('switched');
+</script>
+<template>
+  <div class="tw-flex tw-items-center">
+    <q-toggle
+      v-model="switched"
+      :label="label"
+      :icon="icon"
+      size="lg"
+      color="primary"
+      dense
+      left-label
+    />
+  </div>
+</template>
+<style>
+.q-btn .q-focus-helper {
+  display: none;
+}
+</style>
