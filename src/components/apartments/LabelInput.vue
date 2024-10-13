851c1777bce83d7bd5e6827ee480538977837f9f
enqer <pawel.kam4@gmail.com>
2024-10-13 14:45:59 +0200

    feat: custom input

---
+++ src/components/apartments/LabelInput.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+defineProps({
+  label: {
+    type: String,
+    required: true,
+  },
+  modelValue: {
+    type: String,
+    required: true,
+  },
+  required: {
+    type: Boolean,
+    required: false,
+  },
+});
+const emits = defineEmits(['update:modelValue']);
+</script>
+<template>
+  <div>
+    <div class="tw-flex tw-gap-x-1 tw-font-medium">
+      {{ label }}
+      <div v-if="required" class="tw-text-red-500">*</div>
+    </div>
+    <q-input
+      :model-value="modelValue"
+      :rules="[(val: string) => val !== '' || 'Nieprawidłowe dane']"
+      outlined
+      dense
+      hide-bottom-space
+      @update:model-value="emits('update:modelValue', $event)"
+    />
+  </div>
+</template>
